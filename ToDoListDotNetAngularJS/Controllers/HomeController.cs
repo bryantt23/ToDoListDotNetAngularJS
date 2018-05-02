be59da9d87733968796d19ff60eb8cde1ca4ec97
bryantt23 <bryantt23@DESKTOP-7I0G27J>
2018-05-01 22:52:23 -0700

    Add project files.

---
+++ ToDoListDotNetAngularJS/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.Mvc;
+
+namespace ToDoListDotNetAngularJS.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpGet]
+        public void GetEmployees()
+        {
+
+            string connectionStr = ConfigurationManager
+                .ConnectionStrings["connectionStr"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(connectionStr))
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = con;
+                cmd.CommandText = "spGetTodos";
+                cmd.CommandType = CommandType.StoredProcedure;
+
+
+                con.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+            }
+        }
+    }
+}
---
+++ ToDoListDotNetAngularJS/Scripts/Todos/Todos.js
@@ -0,0 +1,20 @@
+﻿/// <reference path="../angular.js" />
+
+var todoApp = angular.module("todoApp", []);
+
+todoApp.controller("todoCtrl", function ($scope, $http) {
+    $scope.message = "hii";
+
+    $scope.nums = [1, 2, 3, 4, 5];
+
+    $scope.getTodos = function () {
+        $http({
+            method: "GET",
+            url: "Home/GetEmployees"
+        }).then(function mySuccess(response) {
+            $scope.myWelcome = response.data;
+        }, function myError(response) {
+            $scope.myWelcome = response.statusText;
+        });
+    }
+});
